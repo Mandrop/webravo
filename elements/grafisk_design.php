dbeac3e53680c2e8869081995df9fb57acf3c36e
GrothDK <61182782+GrothDK@users.noreply.github.com>
2020-04-21 12:27:03 +0200

    grafisk design sektion

---
+++ elements/grafisk_design.php
@@ -0,0 +1,16 @@
+<!-- GRAFISK DESIGN SEKTION -->
+
+<div class="container-fluid p-0 my-5 grafisk-design">
+
+    <div class="container-fluid bg-webravo-blue p-5 d-flex flex-column justify-content-center grafisk-design-skew text-white">
+        <article class="grafisk-design-skew-back my-5">
+            <h1 class="text-center">Grafisk design</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat tempora facere tenetur libero voluptas, voluptatem nostrum consequatur eligendi cum exercitationem dignissimos iure laudantium placeat est corrupti similique? Quaerat hic, voluptatem eum ducimus laboriosam tenetur maiores temporibus qui mollitia facere quisquam exercitationem placeat magnam, laudantium quos provident, culpa expedita blanditiis. Perferendis?</p> 
+        </article>   
+    </div>
+
+
+</div>
+
+<!-- GRAFISK DESIGN SEKTION END -->
+
