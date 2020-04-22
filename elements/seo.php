9844c0f0d2973764d2387da69b4a446c579a16c0
GrothDK <61182782+GrothDK@users.noreply.github.com>
2020-04-22 11:56:52 +0200

    SEO

---
+++ elements/seo.php
@@ -0,0 +1,22 @@
+<!-- GRAFISK DESIGN SEKTION -->
+
+<div id="grafiskdesign" class="container-fluid p-0 grafisk-design">
+
+    <div class="container-fluid bg-webravo-blue p-5 d-flex flex-column justify-content-center grafisk-design-skew text-white">
+        <article class="grafisk-design-skew-back my-5">
+            <h1 class="text-center">SEO</h1>
+            <p><br>
+            Hos Webravo får du ikke bare en standard skabelon baseret hjemmeside. <br>
+Vi skaber, dit helt eget unikke digitale design på nettet, i tæt samarbejde med dig og din forretnings kerne værdier. <br>
+<br>
+Designet skal være personligt og appetitvækkende, samtidigt skal det selvfølgelig være responsivt, så det tager sig godt ud på alle de skærme, dine kunder ser hjemmesiden på. <br>
+<br>
+Hjemmesiden skal udtrykke dit budskab og give kunden overskuelighed og let mulighed for interaktion og henvendelse, det skaber konverteringer.</p> 
+        </article>   
+    </div>
+
+
+</div>
+
+<!-- GRAFISK DESIGN SEKTION END -->
+
