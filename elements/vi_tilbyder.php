7a8a5182d906450f6457d34e74c4d0b389413813
GrothDK <61182782+GrothDK@users.noreply.github.com>
2020-04-22 11:09:38 +0200

    update

--- elements/vi_tilbyder.php
+++ elements/vi_tilbyder.php
@@ -8,7 +8,7 @@
     </div>
 
     <article class="container border text-left p-4">
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, quas dolorum quasi molestias minus debitis aliquid odit accusamus quos, soluta tenetur ad illum veritatis officia. Maiores, nam veritatis ea in cupiditate consequuntur ut numquam, doloribus quidem libero corporis non sunt id eos, a laudantium beatae? Cumque aliquam at iste pariatur natus, totam odit voluptatum officiis veritatis cupiditate nulla doloribus dolore magni. Accusantium iure maiores cum. Incidunt, omnis officiis cupiditate rem corporis accusamus vitae eius magni a quidem explicabo magnam pariatur repellat in. Temporibus veritatis ipsam labore. Architecto voluptas corrupti quas soluta cum, corporis optio incidunt cupiditate. Id fuga ex numquam!</p>
+        <p>Din hjemmeside er ofte din repræsentant for din virksomhed. Det er ofte det første en kommende kunde møder, og derfor skal dit webdesign se godt ud. Vi ved at et veludviklet design skaber kunder og øger din konvertering markant. Derfor vægter vi dette punkt højt. Vi er specialistister i design udvikling, og har flere års erfaring.</p>
     </article>
 
 
