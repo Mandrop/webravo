ece6df59f70f0df0cd35534e17583705b2045294
Frederikhofstedt <63840727+Frederikhofstedt@users.noreply.github.com>
2020-04-22 10:53:07 +0200

    omos

---
+++ elements/omos.php
@@ -0,0 +1,38 @@
+<!-- Om os Sektion starter -->
+  <section class="omos-overskrift">
+    <h1>Om os</h1>
+  </section>
+  <section class="omosall">
+    <section class="omos">
+        <div> <img src="images/4.png"> 
+        <p class="omos-navn">Tobias Herholdt</p>
+        <p class="omos-title">Grafisk Designer</p>
+        <p class="omos-mail">Herholdt@mail.com</p>
+        </div>
+        
+        <div> <img src="images/5.png"> 
+        <p class="omos-navn">Frederik Holstad</p>
+        <p class="omos-title">Direktør</p>
+        <p class="omos-mail">Holstad@mail.com</p>
+        </div>
+        
+        <div> <img src="images/3.png">
+        <p class="omos-navn">Henrik Groth</p>
+        <p class="omos-title">Alt Mulig Mand</p>
+        <p class="omos-mail">Groth@mail.com</p>
+        </div>
+       
+        <div> <img src="images/2.png"> 
+        <p  class="omos-navn">Lukas Larsen</p>
+        <p class="omos-title">Webudvikler</p>
+        <p class="omos-mail">Larsen@mail.com</p>
+        </div>
+        
+        <div> <img src="images/1.png"> 
+        <p  class="omos-navn">Frederik Hofstedt</p>
+        <p class="omos-title">Grafisk Designer</p>
+        <p class="omos-mail">Hofstedt@mail.com</p>
+        </div>
+    </section>
+    </section>
+<!-- Om os Sektion stopper -->
