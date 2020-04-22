0fa1189685da36027d76f0fa83074f8b88589b5d
lordlukas10 <lukas.tipsmark@gmail.com>
2020-04-22 13:19:08 +0200

    .

--- index.php
+++ index.php
@@ -48,12 +48,14 @@ include('elements/seo.php');
 ?>
 </div>
 
-<script src="javascript.js"></script>
-
+<div id="omos">
 <?php 
 include('elements/omos.php');
 ?>
+</div>
 
 <?php
 include('elements/footer.php');
 ?>
+
+<script src="javascript.js"></script>
