1680524b8ccb7a4a123c2c4c1e694f0d540735f5
GrothDK <61182782+GrothDK@users.noreply.github.com>
2020-04-23 11:45:36 +0200

    Js update

--- javascript.js
+++ javascript.js
@@ -2,41 +2,54 @@ window.onscroll = function(){
  /*    console.log(window.pageYOffset); */
 	if(window.pageYOffset > 180 && window.pageYOffset < 700){
 		document.getElementById('navlink1').style.color ="#2cafe4";
+		document.getElementById('navlink1').style.borderBottom="2px solid #2cafe4";
+
+
 
 	}else{
 		document.getElementById('navlink1').style.color ="rgba(0,0,0,.5)";
+		document.getElementById('navlink1').style.borderBottom="2px solid transparent";
+
 
 	}
     
     if(window.pageYOffset > 700 && window.pageYOffset < 1515){
 		document.getElementById('navlink2').style.color ="#2cafe4";
+		document.getElementById('navlink2').style.borderBottom="2px solid #2cafe4";
 
 	}else{
 		document.getElementById('navlink2').style.color ="rgba(0,0,0,.5)";
+		document.getElementById('navlink2').style.borderBottom="2px solid transparent";
 
 	}
 
     if(window.pageYOffset > 1713 && window.pageYOffset < 2150){
 		document.getElementById('navlink3').style.color ="#2cafe4";
+		document.getElementById('navlink3').style.borderBottom="2px solid #2cafe4";
 
 	}else{
 		document.getElementById('navlink3').style.color ="rgba(0,0,0,.5)";
+		document.getElementById('navlink3').style.borderBottom="2px solid transparent";
 
 	}
 
     if(window.pageYOffset > 2160 && window.pageYOffset < 3250){
 		document.getElementById('navlink4').style.color ="#2cafe4";
+		document.getElementById('navlink4').style.borderBottom="2px solid #2cafe4";
 
 	}else{
 		document.getElementById('navlink4').style.color ="rgba(0,0,0,.5)";
+		document.getElementById('navlink4').style.borderBottom="2px solid transparent";
 
 	}
 
     if(window.pageYOffset > 3250) {
 		document.getElementById('navlink5').style.color ="#2cafe4";
+		document.getElementById('navlink5').style.borderBottom="2px solid #2cafe4";
 
 	}else{
 		document.getElementById('navlink5').style.color ="rgba(0,0,0,.5)";
+		document.getElementById('navlink5').style.borderBottom="2px solid transparent";
 
 	}
     
