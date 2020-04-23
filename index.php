079cd33f604a28de1b2d4b50a805ecbb9de0536a
GrothDK <61182782+GrothDK@users.noreply.github.com>
2020-04-23 09:22:51 +0200

    update picture

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ include('elements/hjemmeside-udvikling.php');
 include('elements/grafisk_design.php');
 ?>
 
-<div class="container position-relative p-0" style="height:100px">
+<div class="container-fluid position-relative p-0" style="height:100px">
 
 <img src="images/pc.png" alt="" style="width:100%;height:700px;position:absolute;top:-250px;z-index:-1;object-fit:cover;">
 
